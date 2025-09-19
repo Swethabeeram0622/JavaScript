f47b5bb5ff79ddc72d4f43ec570b2f9f907b7f64
Swethabeeram0622 <swethabeeram2022ists@gmail.com>
2025-09-19 13:43:51 +0530

    Set time out.js

---
+++ Set time out.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Set time out</title>
+</head>
+<body>
+    <script>
+        function greet(){
+            console.log(`hello`)
+        }
+        function greet2(){
+            console.log(`welcome to swetha's world`)
+        }
+        setTimeout(300,greet(),greet2())
+
+    </script>
+</body>
+</html>
