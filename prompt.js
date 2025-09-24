5d2d5c6e1350463c86b577cbb45d1df5cc919522
Swethabeeram0622 <swethabeeram2022ists@gmail.com>
2025-09-24 11:28:22 +0530

    Create prompt.js

---
+++ prompt.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>java script</title>
+</head>
+<body>
+    <script>
+        name=prompt("what is your name")
+        prompt(name)
+        let number=""
+        while(number === ""){
+            number=prompt("enter u r number")
+        }
+    </script>
+</body>
+</html>
