0664c31f24bc0116a35a5e80cee038b5faddbf31
Swethabeeram0622 <swethabeeram2022ists@gmail.com>
2025-09-24 11:29:06 +0530

    Create Alert.js

---
+++ Alert.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>while loop</title>
+</head>
+<body>
+    <script>
+    let number = " "
+    while(number === " ") {
+      alert("please enter u r num")
+    }
+    alert("ok")
+    </script>
+</body>
+</html>
